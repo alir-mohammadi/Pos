de36e253f2ec797339001222f1110fc658862fe6
armoahmmadi <ar_mohammady@yahoo.com>
2017-08-09 19:40:52 +0430

    Store 0.1

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function ReturnWood ()
+    {
+        
+    }
+
+    public function Pay ()
+    {
+        
+    }
+
+    public function AddCustomer ()
+    {
+        
+    }
+
+    public function EditJar ()
+    {
+        
+    }
+    public function DeleteJar()
+    {
+        
+    }
+
+    public function MakeBill ()
+    {
+        
+    }
+
+    public function PrintBail ()
+    {
+        
+    }
+
+    public function CustomerScreen ()
+    {
+        
+    }
+}
---
+++ app/Store.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Wood;
+class Store extends Model
+{
+
+}
